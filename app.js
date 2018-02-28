4d94c719912f3081c2db28eb091e550556db913a
Keith Rodriguez <rodriguez.keith@gmail.com>
2018-02-27 23:57:12 -0500

    Refactor routes, add login and register pages

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const exphbs = require('express-handlebars');
 const methodOverride = require('method-override'); // for PUT and DELETE methods
 const flash = require('connect-flash'); // flash messages
@@ -7,6 +8,10 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const app = express();
+
+// Load Routes
+const ideas = require('./routes/ideas');
+const users = require('./routes/users');
 
 // Map global promise
 mongoose.Promise = global.Promise;
@@ -17,10 +22,6 @@ mongoose.connect('mongodb://localhost/jot-a-thought', {
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
 
-// Load Idea Model / Schema
-require('./models/Idea');
-const Idea = mongoose.model('ideas');
-
 // Handlebars Middleware
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
@@ -28,6 +29,9 @@ app.set('view engine', 'handlebars');
 // Body Parser Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Method Override Middleware
 app.use(methodOverride('_method'));
@@ -67,96 +71,11 @@ app.get('/about', (req, res) => {
   res.render('about');
 });
 
-// Ideas Index Page
-app.get('/ideas', (req, res) => {
-  // Retreive all the ideas
-  Idea.find({})
-    .sort({date: 'desc'})
-    .then(ideas => {
-      res.render('ideas/index', {
-        ideas: ideas
-      });
-    });
-});
-
-// Add Idea Form
-app.get('/ideas/add', (req, res) => {
-  res.render('ideas/add');
-});
-
-// Edit Idea Form
-app.get('/ideas/edit/:id', (req, res) => {
-  // Find the idea by id
-  Idea.findOne({
-    _id: req.params.id
-  })
-  // Render page with found idea
-  .then(idea => {
-    res.render('ideas/edit', {
-      idea: idea
-    });
-  })
-});
-
-// Process Form
-app.post('/ideas', (req, res) => {
-  let errors = [];
-
-  if(!req.body.title) {
-    errors.push({ text: 'Please add a title'})
-  }
-  if(!req.body.details) {
-    errors.push({ text: 'Please add a details'})
-  }
-
-  // If errors rerender form with the original data and the errors
-  if(errors.length > 0) {
-    res.render('ideas/add', {
-      errors: errors,
-      title:req.body.title,
-      details: req.body.details
-    });
-  } else {
-    const newUser = {
-      title: req.body.title,
-      details: req.body.details,
-    }
-    new Idea(newUser)
-      .save()
-      .then(idea => {
-        req.flash('success_msg', 'Idea note added');
-        res.redirect('/ideas');
-      });
-  }
-});
-
-// Edit Form process
-app.put('/ideas/:id', (req, res) => {
-  Idea.findOne({
-    _id: req.params.id
-  })
-  .then(idea => {
-    // new values
-    idea.title = req.body.title;
-    idea.details = req.body.details;
-
-    // save new data
-    idea.save()
-      .then(idea => {
-        req.flash('success_msg', 'Idea note updated');
-        res.redirect('/ideas');
-      })
-  })
-});
-
-// Delete Idea
-app.delete('/ideas/:id', (req, res) => {
-  Idea.remove({_id: req.params.id})
-    .then(() => {
-      req.flash('success_msg', 'Idea note removed');
-      res.redirect('/ideas');
-    })
-})
+// ===========================
+// Use Routes from other files
+// ===========================
+app.use('/ideas', ideas);
+app.use('/users', users);
 
 const port = 5000;
 
