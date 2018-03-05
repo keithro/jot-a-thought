de360d0276391e4264d4a3c1ee2bfe663450fed1
Keith Rodriguez <rodriguez.keith@gmail.com>
2018-03-05 00:58:45 -0500

    New user registration

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+// const passport = require('passport');
 const router = express.Router();
+
+// Load User Model
+require('../models/User');
+const User = mongoose.model('users');
 
 // User Login Route
 router.get('/login', (req, res) => {
@@ -10,6 +16,59 @@ router.get('/login', (req, res) => {
 // User Register Route
 router.get('/register', (req, res) => {
   res.render('users/register');
+});
+
+// Register Form POST
+router.post('/register', (req, res) => {
+  let errors = [];
+
+  if(req.body.password !== req.body.password2) {
+    errors.push({text: 'Passwords do not match'});
+  }
+  if(req.body.password.length < 4) {
+    errors.push({text: 'Password must be at least four characters'});
+  }
+
+  if(errors.length) {
+    // Populate previous data if errors
+    res.render('users/register', {
+      errors: errors,
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      password2: req.body.password2
+    });
+  } else {
+    User.findOne({email: req.body.email})
+      .then(user => {
+        if(user) {
+          req.flash('error_msg', 'Email already registered');
+          res.redirect('/users/register');
+        } else {
+          const newUser = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+          });
+
+          bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(newUser.password, salt, (err, hash) => {
+              if (err) throw err;
+              newUser.password = hash; // set the pw equal to the hashed pw
+              newUser.save()
+                .then(user => {
+                  req.flash('success_msg', 'You are now registered and can log in');
+                  res.redirect('/users/login');
+                })
+                .catch(err => {
+                  console.log(err);
+                  return;
+                });
+            });
+          });
+        }
+      })
+  }
 });
 
 module.exports = router;
